da49538507b92fc03d6c461934dd80460f729932
prafulbagai <praful.bagai1991@gmail.com>
2020-09-09 13:18:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(name='seynse_logger',
       author_email='niranjan@seynse.com',
       description='Logging package to log formatted logs so that its readable via kibana, '
                   'specifically designed for seynse.',
+      install_requires=[
+            'arrow==0.16.0',
+            'pytz==2020.1'
+      ]
       packages=find_packages('.', exclude='logger_test'),
       long_description=open('README.md').read(),
       zip_safe=False)  # As there is only one package `seynse_logger` zipping it won't effect much.
