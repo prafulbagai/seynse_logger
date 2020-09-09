070d8322fb0e92a256749ecaeb283ecd43852f11
prafulbagai <praful.bagai1991@gmail.com>
2020-09-09 12:48:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,5 @@ setup(name='seynse_logger',
       description='Logging package to log formatted logs so that its readable via kibana, '
                   'specifically designed for seynse.',
       packages=find_packages('.', exclude='logger_test'),
-      install_requires=[
-          'Django>=1.8,<2',
-          'arrow>=0.12.1',
-          'pytz>=2017.3',
-      ],
       long_description=open('README.md').read(),
       zip_safe=False)  # As there is only one package `seynse_logger` zipping it won't effect much.
